3f0d0330cfeb77d45ddce44f48b8563fbbe9f781
icepants <shawnluther99@gmail.com>
2019-07-30 21:35:54 -0400

    Created the basic MVC file structure for this project.

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+USE burgers_db;
+
+SELECT * FROM burgers;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheddar Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Pepperjack Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Plain Burger", true);
