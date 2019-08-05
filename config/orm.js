8377a9bfbc2480cfa1492457d1c03cd05cb21a1b
icepants <shawnluther99@gmail.com>
2019-08-05 15:28:28 -0400

    Performed a code cleanup, commented code to give explanations of how things work.

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,20 @@
 // Require SQL connection.
 const connection = require("./connection");
+
+// Creates a data placeholder for our orm insert.
 function createQmarks(num) {
+    // Create an empty array.
     var arr = [];
+    // Loop through our hum arg.
     for (var i = 0; i < num; i++) {
-      arr.push("?");
+        // Push "?" into our ary.
+        arr.push("?");
     }
+    // Return our "arr" in a "string" format.
     return arr.toString();
-  }
+}
+
+// create our orm object.
 var orm = {
     // * `selectAll()`.
     selectAll: function (table, cb) {
@@ -22,7 +30,9 @@ var orm = {
             cb(res);
         });
     },
+    // Using our orm method, use "insertOne" to insert one record into the database.
     insertOne: function (table, cols, vals, cb) {
+        // SQL statement saying "insert into 'burgers_db' with the value of (?)".
         var dbQuery =
             "INSERT INTO " +
             table +
@@ -32,12 +42,16 @@ var orm = {
             "VALUES (" +
             createQmarks(vals.length) +
             ") ";
-
+        // Log the user entered data into the console.
         console.log(dbQuery);
+
+        // Put this data through a connetion query.
         connection.query(dbQuery, vals, function (err, res) {
+            // If there are errors, handle it.
             if (err) {
                 throw err;
             }
+            // Execute response callback.
             cb(res);
         });
     },
