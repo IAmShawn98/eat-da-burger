fd2b4c847890d564badaf3d6263db7ae3f893a76
icepants <shawnluther99@gmail.com>
2019-08-06 19:48:57 -0400

    Added JAWS to my connection for Heroku deployment.

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,16 @@
 var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db",
+    });
+};
 
 connection = mysql.createConnection({
     host: "localhost",
@@ -7,7 +19,7 @@ connection = mysql.createConnection({
     database: "burgers_db",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error("Error connecting " + err.stack);
         return;
